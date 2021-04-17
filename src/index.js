c8ba190b5a01c77be2333976e0ea80a5cb583ec1
MariomcgeeArt <mariomcgeeart@gmail.com>
2021-04-17 15:11:32 -0500

    challenge4s 2 and 3 completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 const str = 'hello world'
 
-
+//Challenge 1
 function capitalize(str) {
-    const first = str[0].toUppercase()
+    const first = str[0].toUpperCase()
     const rest = str.slice(1)
     return first + rest 
 }
 
 
 console.log(capitalize(str))
+
+//Challenge 2
+function allcaps(str) {
+    return str.toUpperCase()
+}
+
+//Challenge 3
+
+function capitalizeWords(str) {
+    const words = tr.split(' ')
+    const upperWords = words.map( word => capitalize(word))
+    return upperWords.join(' ')
+
+
+}
