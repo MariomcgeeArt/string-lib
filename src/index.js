a833ef0ef8cf3aea25b7b9139429d6b41074f3c7
MariomcgeeArt <mariomcgeeart@gmail.com>
2021-04-18 19:29:39 -0500

    further functions completed

--- src/index.js
+++ src/index.js
@@ -32,3 +32,38 @@ function removeExtraSpaces(str) {
     const filtered = chars.filter( (c) => c !== '')
     return filtered.join(' ')
 }
+
+//challenge 5 
+function kebobCase(str, separator = '-') {
+    const lowercase = str.toLowerCase()
+    const characters = lowercase.split('')
+    const filtered =  characters.filter( (c) => {
+        const code = c.characterCodeAt(0)
+        if (code > 96 && code < 123) { // keep lower-case letters
+            return True
+        } else if (code > 47 && code < 58 ){ //keep numbers
+            return True
+        } else if (code === 45 || code === 32 ){ // keep space and hyphen 
+            return True
+        }
+        return False
+    })
+
+    const spaceFree = removeExtraSpaces(filtered.join(separator))
+
+    return spaceFree.split(' ').join('-')
+}
+
+
+//challenge 6
+function snakeCase(str) {
+    return kebeob(str, '_')
+
+
+} 
+
+
+    const spaceFree = removeExtraSpaces(filtered.join(separator))
+
+    return spaceFree.split(' ').join('-')
+}
