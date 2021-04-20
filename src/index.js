ec2ad89b572739742ad57b9f95d34c988c9f1baa
MariomcgeeArt <mariomcgeeart@gmail.com>
2021-04-20 14:37:00 -0500

    finished library

--- src/index.js
+++ src/index.js
@@ -43,12 +43,11 @@ function kebobCase(str, separator = '-') {
             return True
         } else if (code > 47 && code < 58 ){ //keep numbers
             return True
-        } else if (code === 45 || code === 32 ){ // keep space and hyphen 
+        } else if (code === separator.charCodeAt(0) || code === 32 ){ // keep space and hyphen 
             return True
         }
         return False
     })
-
     const spaceFree = removeExtraSpaces(filtered.join(separator))
 
     return spaceFree.split(' ').join('-')
@@ -58,12 +57,72 @@ function kebobCase(str, separator = '-') {
 //challenge 6
 function snakeCase(str) {
     return kebeob(str, '_')
-
-
 } 
 
+//challenge 7
+function camelCase(str) {
+    const words = str.split(' ')
+    const camelWords= words.map((word, i) => {
+        if (i === 0) {
+            return word.toLowerCase()
+        }
+        return capitalize(word)
+    })
 
-    const spaceFree = removeExtraSpaces(filtered.join(separator))
-
-    return spaceFree.split(' ').join('-')
+    return camelWords.join('')
 }
+
+//challenge 8 
+function shift(str) {
+    const first = str[0]
+    const rest = str.split(1)
+    return rest + first 
+}
+
+//challenge 9 
+function makeHashTag(str) {
+    let wordArray = str.split(" ").filter((char) => char !== "");
+    wordArray.sort((a, b) => b.length - a.length);
+    let topThree = wordArray.slice(0,3)
+    
+  
+    // console.log(wordArray)
+    let result = "#";
+    
+    if (wordArray.length === 0) {
+      return false;
+    }
+  
+  
+    result =
+      
+      topThree
+        .map((word) => {
+          let capitalizedWord = result + word.charAt(0).toUpperCase() + word.slice(1);
+          return capitalizedWord;
+        });
+        
+        
+  
+    if (result.length > 140) {
+      return false;
+    } else {
+      return result;
+    }
+  }
+  
+  console.log(" ");
+  console.log("Make Hashtag:");
+  str = "Amazing bongo drums for sale";
+  console.log(makeHashTag(str));
+  
+//challenge 10
+  
+  function isEmpty(str) {
+    for (let i = 0; i < str.length; i += 1) {
+      if (str[i] !== ' ' || str[i]  !== ('\n') || str[i] !== ('\r') || str[i] !== ('\t')) {
+        return false 
+      }
+    }
+    return true
+  }
